c1f771f6e3047e03f02094fd97e87f43813ad887
Sam Felsted <61361381+SamFelsted@users.noreply.github.com>
2025-03-06 00:26:20 -0800

    long distance

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@ Adafruit_VL53L0X lox = Adafruit_VL53L0X();
 
 NetworkInterface networkInterface(80);
 
+VL53L0X_RangingMeasurementData_t measure;
 
 void scanI2C() {
     Serial.println("Scanning I2C...");
@@ -27,6 +28,7 @@ void scanI2C() {
 void setup() {
     Serial.begin(115200);
     Wire.begin(21, 22);
+    Wire.setClock(50000);
     scanI2C();
 
     networkInterface.boot();
@@ -36,14 +38,26 @@ void setup() {
         delay(1);
     }
 
-    Serial.println("Adafruit VL53L0X test");
+    Serial.println("Adafruit VL53L0X test.");
 
     delay(1000);
-    if (!lox.begin()) {
+    if (!lox.begin(0x29, true)) {
         Serial.println(F("Failed to boot VL53L0X"));
-        delay(1);
+        while(true);
     }
-    Serial.println(uxTaskGetStackHighWaterMark(NULL));  // Print free stack space
+
+    Serial.println(F("VL53L0X API Continuous Ranging example\n\n"));
+
+    //lox.setDeviceMode(Adafruit_VL53L0X::VL53L0X_SENSE_LONG_RANGE);
+    lox.configSensor(Adafruit_VL53L0X::VL53L0X_SENSE_LONG_RANGE);
+
+
+    //lox.startRangeContinuous();
+    lox.setMeasurementTimingBudgetMicroSeconds(200000);  // 200ms for better range detection
+
+    Serial.println(uxTaskGetStackHighWaterMark(nullptr));  // Print free stack space
+
+
 
     Serial.println("VL53L0X ready");
 }
@@ -51,18 +65,16 @@ void setup() {
 
 
 void loop() {
-    VL53L0X_RangingMeasurementData_t measure;
-
     Serial.print("Reading a measurement... ");
-    lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
+    lox.rangingTest(&measure, true); // pass in 'true' to get debug data printout!
 
     if (measure.RangeStatus != 4) {  // phase failures have incorrect data
-        Serial.print("Distance (mm): "); Serial.println(measure.RangeMilliMeter);
+        Serial.print("Distance (mm): ");
+        Serial.println(measure.RangeMilliMeter);
         networkInterface.data = measure.RangeMilliMeter;
     } else {
         Serial.println(" out of range ");
     }
-
-    delay(100);
+    delay(500);
 
 }
